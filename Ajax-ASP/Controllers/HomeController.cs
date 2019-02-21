0c63719c207f38c8b2ebb23923cea5c433d41808
Ngô Trung Đức <35600894+ogggy4858@users.noreply.github.com>
2019-02-21 21:30:48 +0700

    Add files via upload

---
+++ Ajax-ASP/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Ajax_ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Ajax_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<EmployeeModel> list = new List<EmployeeModel>()
+        {
+            new EmployeeModel()
+            {
+                 ID = 1,
+                Name = "Duc",
+                Salary = 10,
+                Status = true
+            },
+             new EmployeeModel()
+             {
+                 ID = 2,
+                Name = "Nam",
+                Salary = 100,
+                Status = true
+             },
+             new EmployeeModel()
+             {
+                 ID = 3,
+                Name = "Tien",
+                Salary = 1000,
+                Status = true
+             }
+        };
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        [HttpGet]
+        public JsonResult LoadData()
+        {
+            return Json(new
+            {
+                data = list,
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Update(string model)
+        {
+            JavaScriptSerializer seria = new JavaScriptSerializer();
+            EmployeeModel mo = seria.Deserialize<EmployeeModel>(model);
+
+            var entity = list.Single(x => x.ID == mo.ID);
+            entity.Salary = mo.Salary;
+            return Json(new
+            {
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Ajax-ASP/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajax_ASP.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public bool Status { get; set; }
+    }
+}
