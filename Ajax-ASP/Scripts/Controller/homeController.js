d7a2ec53bbe1100b416efef598ff010c49cc6c72
Ngô Trung Đức <35600894+ogggy4858@users.noreply.github.com>
2019-02-21 21:32:05 +0700

    Add files via upload

---
+++ Ajax-ASP/Scripts/Controller/homeController.js
@@ -0,0 +1,39 @@
+﻿var homeController = {
+    init: function () {
+        homeController.loadData();
+      
+    },
+    registerEvent: function () {
+      
+        $('.txtSalary').off('keypress').on('keypress', function (e) {
+            if (e.which == 13) {
+                alert(1);
+            }
+        });
+    },
+    loadData: function () {
+        $.ajax({
+            url: 'Home/LoadData',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                if (response.status) {
+                    var data = response.data;
+                    var html = '';
+                    var template = $('#data-template').html();
+                    $.each(data, function (i, item) {
+                        html = html + Mustache.render(template, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Salary: item.Salary,
+                            Status: item.Status == true ? "<span class = \"label label-success\">Active</span>"  : "<span class = \"label label-danger\">Locked</span>"
+                        });
+                    });
+                    $('#tblData').html(html);
+                    homeController.registerEvent();
+                }
+            }
+        });
+    }
+};
+homeController.init();
---
+++ Ajax-ASP/Scripts/Controller/homeController2.js
@@ -0,0 +1,67 @@
+﻿var home = {
+    init: function () {
+        home.loadData();
+
+    },
+    registerEvent: function () {
+        $('.txtSalary').off('keypress').on('keypress', function (e) {
+            if (e.which == 13) {
+                var id = $(this).data('id');
+                var value = $(this).val();
+                home.updataSalary(id, value);
+            }
+        });
+    },
+    updataSalary: function (id, value) {
+        var aaa = {
+            ID: id,
+            Salary: value
+        };
+        $.ajax({
+            url: '/Home/Update',
+            type: 'POST',
+            dataType: 'json',
+            data: { model: JSON.stringify(aaa) },
+            success: function(response) {
+                if (response.status) {
+                    alert('update success.');
+                }
+                else {
+                    alert('update false.');
+                }
+            }
+        });
+    },
+
+    loadData: function () {
+        $.ajax({
+            url: 'Home/LoadData',
+            type: 'GET',
+            dataType: 'json',
+            success: function (respone) {
+                if (respone.status) {
+                    var data = respone.data;
+                    var html = '';
+                    var template = $('#data-template').html();
+                    $.each(data, function (i, item) {
+                        html += Mustache.render(template, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Salary: item.Salary,
+                            Status: item.Status == true ? "<span class = \"label label-success\">Active</span>" : "<span class = \"label label-danger\">Locked</span>"
+                        });
+                    });
+                    $('#tblData').html(html);
+                    home.registerEvent();
+                }
+                else {
+                    alert('False');
+                }
+            }
+        })
+
+    }
+};
+
+home.init();
+
